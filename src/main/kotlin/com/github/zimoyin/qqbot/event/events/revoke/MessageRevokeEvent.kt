5d46aff9df5ff55d316e0d0ad349cd38f9c3e00b
zimoyin <2556608754@qq.com>
2024-01-06 11:46:14 +0800

    移除未实现TODO：MessageRevokeEvent

--- src/main/kotlin/com/github/zimoyin/qqbot/event/events/revoke/MessageRevokeEvent.kt
+++ src/main/kotlin/com/github/zimoyin/qqbot/event/events/revoke/MessageRevokeEvent.kt
@@ -22,11 +22,6 @@ interface MessageRevokeEvent : Event {
     override val botInfo: BotInfo
     val msgID: String
     val windows: Contact
-    //TODO 添加Get方法从本地的聊天库里面查找消息。该聊天库为键值对形式，key为msgID，value为MessageChain对象。每个群/会话/频道对应一个聊天库
-    //TODO 不具体实现聊天库，而是提供一个接口，最后通过该接口获取实现。实现由用户开发 使用 SPL 加载
-    val messageChain: MessageChain
-        get() = TODO("Not yet implemented")
-
     /**
      * 信息发送者
      */
