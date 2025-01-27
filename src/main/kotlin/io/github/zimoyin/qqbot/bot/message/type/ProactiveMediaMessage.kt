8c694557a4e5e29f01a6261dd0ff124874e773fe
zimoyin <2556608754@qq.com>
2025-01-27 15:55:12 +0800

    feat(message): 添加 ProactiveMediaMessage 类的说明注释

--- src/main/kotlin/io/github/zimoyin/qqbot/bot/message/type/ProactiveMediaMessage.kt
+++ src/main/kotlin/io/github/zimoyin/qqbot/bot/message/type/ProactiveMediaMessage.kt
@@ -5,6 +5,7 @@ import io.github.zimoyin.qqbot.net.bean.message.send.MediaMessageBean
 
 /**
  * 主动发送资源消息，只能发不能收
+ * 群聊/单人复用已经已经上传的文件
  */
 class ProactiveMediaMessage(val media: MediaMessageBean) : MessageItem {
     override fun toStringType(): String {
